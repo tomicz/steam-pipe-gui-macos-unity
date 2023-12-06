6e7327354f1a6b161ce7c45f7ced49aca640d9ef
Darko Tomic <tomiczdarko@gmail.com>
2023-12-06 04:29:48 +0100

    Added scripts that generate VDF app_ and depot_ files in your scripts folder

---
+++ Editor/DeploymentConfigurator.cs
@@ -0,0 +1,65 @@
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Birdmoot.Settings
+{
+    [CreateAssetMenu(fileName = "DeploymentConfigurator", menuName = "Tomicz/Steam/Depoloyement Target")]
+    public class DeploymentConfigurator : ScriptableObject
+    {
+        public BuildTarget BuildTarget => _buildTarget;
+        public string Description => _description;
+        public string AppName => _appName;
+        public string SteamUsername => _steamUsername;
+        public string sdkPath { get; set; }
+        public string AppId => _appId;
+        public string DepotId => _depotId;
+
+        [SerializeField] private BuildTarget _buildTarget;
+
+        [Header("App info")]
+        [SerializeField] private string _appName = "";
+        [SerializeField] private string _description = "";
+
+        [Header("Steamworks info")]
+        [SerializeField] private string _steamUsername;
+        [SerializeField] private string _appId;
+        [SerializeField] private string _depotId;
+
+        public void OnBuildTargetClicked()
+        {
+            string outputPath = Path.Combine(sdkPath, "tools", "ContentBuilder", "content", $"{_buildTarget}", $"{_appName}{GetAppExecutionExtension()}");
+
+            BuildPipeline.BuildPlayer(GetScenePaths(), outputPath, _buildTarget, BuildOptions.None);
+
+            Debug.Log((int)_buildTarget);
+            Debug.Log("Target successfully built.");
+        }
+
+        private string GetAppExecutionExtension()
+        {
+            if(_buildTarget == BuildTarget.StandaloneOSX)
+            {
+                return ".app";
+            }
+            else if(_buildTarget == BuildTarget.StandaloneWindows)
+            {
+                return ".exe";
+            }
+
+            return ".exe";
+        }
+
+        private static string[] GetScenePaths()
+        {
+            string[] scenes = new string[EditorBuildSettings.scenes.Length];
+
+            for (int i = 0; i < scenes.Length; i++)
+            {
+                scenes[i] = EditorBuildSettings.scenes[i].path;
+            }
+
+            return scenes;
+        }
+    }
+}
---
+++ Editor/DeploymentConfiguratorEditor.cs
@@ -0,0 +1,178 @@
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using UnityEngine;
+
+namespace Birdmoot.Settings
+{
+    [CustomEditor(typeof(DeploymentConfigurator))]
+    public class DeploymentConfiguratorEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUILayout.Space(10);
+
+            DeploymentConfigurator deploymentConfigurator = (DeploymentConfigurator)target;
+
+            GUILayout.BeginHorizontal();
+
+            GetSDKPath(deploymentConfigurator);
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.Space(10);
+
+            GenerateDepotFile(deploymentConfigurator);
+
+            GUILayout.Space(10);
+
+            BuildTarget(deploymentConfigurator);
+
+            GUILayout.Space(10);
+        }
+
+        private void GenerateDepotFile(DeploymentConfigurator deploymentConfigurator)
+        {
+            if(GUILayout.Button("Generate Depot File"))
+            {
+                string depotVDFPath = Path.Combine(deploymentConfigurator.sdkPath, "tools", "ContentBuilder", "scripts", $"app_{deploymentConfigurator.DepotId}.vdf");
+
+                if (!File.Exists(depotVDFPath))
+                {
+                    File.WriteAllText(depotVDFPath, GetDepotContent(deploymentConfigurator));
+                }
+            }
+
+            UpdateDepotVDF(deploymentConfigurator);
+        }
+
+        private string GetDepotContent(DeploymentConfigurator deploymentConfigurator)
+        {
+            string vdfContent = "appbuild\n{\n";
+
+            vdfContent += $"\t\"appid\" \"{deploymentConfigurator.AppId}\"\n";
+            vdfContent += $"\t\"desc\" \"{deploymentConfigurator.Description}\"\n";
+            vdfContent += $"\t\"buildoutput\" \"{Path.Combine(deploymentConfigurator.sdkPath, "tools", "ContentBuilder", "output", deploymentConfigurator.BuildTarget.ToString())}\" // Replace this with the correct property\n";
+            vdfContent += "\t\"contentroot\" \"\"\n";
+            vdfContent += "\t\"setlive\" \"beta\"\n";  // You can modify this line if needed
+            vdfContent += "\t\"preview\" \"0\"\n";
+            vdfContent += "\t\"local\" \"\"\n";
+            vdfContent += "\t\"depots\"\n\t{\n";
+            vdfContent += $"\t\t\"{deploymentConfigurator.DepotId}\" \"{Path.Combine(deploymentConfigurator.sdkPath, "tools", "ContentBuilder", "scripts", $"depot_{deploymentConfigurator.DepotId}.vdf")}\"\n";
+            vdfContent += "\t}\n}";
+
+            return vdfContent;
+        }
+
+        private void UpdateDepotVDF(DeploymentConfigurator deploymentConfigurator)
+        {
+            string depotVDFPath = Path.Combine(deploymentConfigurator.sdkPath, "tools", "ContentBuilder", "scripts", $"depot_{deploymentConfigurator.DepotId}.vdf");
+
+            File.WriteAllText(depotVDFPath, GetDepotBuildConfigContent(deploymentConfigurator));
+        }
+
+        private string GetDepotBuildConfigContent(DeploymentConfigurator deploymentConfigurator)
+        {
+            string depotBuildConfigContent = "DepotBuildConfig\n{\n";
+
+            depotBuildConfigContent += $"\t\"DepotID\" \"{deploymentConfigurator.DepotId}\"\n";
+            depotBuildConfigContent += $"\t\"contentroot\" \"{Path.Combine(deploymentConfigurator.sdkPath, "tools", "ContentBuilder", "content", deploymentConfigurator.BuildTarget.ToString())}\"\n";
+            depotBuildConfigContent += "\t\"FileMapping\"\n\t{\n";
+            depotBuildConfigContent += "\t\t\"LocalPath\" \"*\"\n";
+            depotBuildConfigContent += "\t\t\"DepotPath\" \".\"\n";
+            depotBuildConfigContent += "\t\t\"recursive\" \"1\"\n";
+            depotBuildConfigContent += "\t}\n";
+            depotBuildConfigContent += "\t\"FileExclusion\" \"*.pdb\"\n";
+            depotBuildConfigContent += "}\n";
+
+            return depotBuildConfigContent;
+        }
+
+        private void GetSDKPath(DeploymentConfigurator deploymentConfigurator)
+        {
+            EditorGUILayout.LabelField("SDK Folder Path:", deploymentConfigurator.sdkPath);
+
+            if (GUILayout.Button("Browse", GUILayout.Width(80)))
+            {
+                string sdkPath = EditorUtility.OpenFolderPanel("Select Folder", "", "");
+
+                if (!string.IsNullOrEmpty(sdkPath))
+                {
+                    deploymentConfigurator.sdkPath = sdkPath;
+
+                    EditorUtility.SetDirty(deploymentConfigurator);
+                }
+            }
+        }
+
+        private void BuildTarget(DeploymentConfigurator deploymentConfigurator)
+        {
+            //UpdateBuildDescription(deploymentConfigurator.sdkPath, deploymentConfigurator.DepotId, deploymentConfigurator.Description);
+
+            if (GUILayout.Button("Build target"))
+            {
+                deploymentConfigurator.OnBuildTargetClicked();
+                RunSteamCommand(deploymentConfigurator.sdkPath, deploymentConfigurator.SteamUsername, deploymentConfigurator.DepotId, deploymentConfigurator.Description, deploymentConfigurator.BuildTarget);
+            }
+        }
+
+        public static void RunSteamCommand(string sdkPath, string username, string depotId, string buildNumber, BuildTarget buildTarget)
+        {
+            string steamCmdCommand = $"{sdkPath}/tools/ContentBuilder/builder_osx/steamcmd.sh +login {username} +run_app_build_http {sdkPath}/tools/ContentBuilder/scripts/app_{depotId}.vdf +quit";
+            string terminalPath = "/System/Applications/Utilities/Terminal.app";
+
+            ProcessStartInfo processStartInfo = new ProcessStartInfo()
+            {
+                FileName = terminalPath,
+                UseShellExecute = true
+            };
+
+            Process.Start(processStartInfo);
+
+            RunSteamCmdCommand(steamCmdCommand);
+        }
+
+        static void RunSteamCmdCommand(string steamCmdCommand)
+        {
+            // Run the SteamCMD command in Terminal
+            ProcessStartInfo runCommandInfo = new ProcessStartInfo()
+            {
+                FileName = "osascript", // osascript is a command-line tool for executing AppleScripts
+                Arguments = $"-e 'tell application \"Terminal\" to do script \"{steamCmdCommand}\"'",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Process.Start(runCommandInfo);
+        }
+
+        public static void UpdateBuildDescription(string sdkPath, string depotId, string description)
+        {
+            string vdfFilePath = Path.Combine(sdkPath, "tools", "ContentBuilder", "scripts", $"app_{depotId}.vdf");
+
+            if (string.IsNullOrEmpty(vdfFilePath))
+            {
+                UnityEngine.Debug.LogError("Couldn't find vdf path");
+                return;
+            }
+
+            // Read the content of the VDF file.
+            string vdfContent = File.ReadAllText(vdfFilePath);
+
+            // Define a regular expression to match the "description" entry.
+            string pattern = "\"desc\"[ \t]+\"[^\"]+\"";
+
+            // Use Regex to find and replace the description entry.
+            vdfContent = Regex.Replace(vdfContent, pattern, $"\"desc\" \"{description}\"");
+
+            // Write the modified content back to the file.
+            File.WriteAllText(vdfFilePath, vdfContent);
+
+            Console.WriteLine("Build description updated.");
+        }
+    }
+}
